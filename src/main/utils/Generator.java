512bc27131baf2157c5dcacee1010571de09f479
alis1 <01187024@pw.edu.pl>
2023-12-23 21:08:07 +0100

    Development moved to development branch

--- src/main/utils/Generator.java
+++ src/main/utils/Generator.java
@@ -1,25 +1,4 @@
 package main.utils;
 
 public class Generator {
-	
-	public static int width;
-	public static int height;
-	public static int pairNumber;
-	
-	int[][] board;
-	
-	public Generator(int width, int height, int pairNumber) {
-		
-		this.width = width;
-		this.height = height;
-		this.pairNumber = pairNumber;
-		
-		board = new int[width][height];
-		
-	}
-	
-	public static void main(String[] args) {
-		
-	}
-	
 }
