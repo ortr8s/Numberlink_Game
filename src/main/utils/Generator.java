76fd27ac7e39636199fcbdaf98dee2a1c38f260c
alis1 <aaaaav@Workstation>
2023-12-23 21:08:40 +0100

    Basic objects and first methods in Generator class.

--- src/main/utils/Generator.java
+++ src/main/utils/Generator.java
@@ -1,4 +1,40 @@
 package main.utils;
 
+import java.util.Random;
+
 public class Generator {
+
+	static Random random = new Random();
+
+	static char[][] board5 = new char[5][5];
+	static char[][] board7 = new char[7][7];
+	static char[][] board9 = new char[9][9];
+
+	public Generator() {
+		System.out.println("Nie wprowadzono wielkości planszy");
+	}
+
+	public Generator(int rozmiar) {
+		//TODO jakis switch
+	}
+
+	public static void main(String[] args) {
+		// TODO usunac printy
+		System.out.println(board5);
+		System.out.println(board7);
+		System.out.println(board9);
+
+	}
+
+	public void generate5(int wielkosc) {
+		System.out.println("Wygenerowano planszę o wielkości 5");
+	}
+
+	public void generate7(int wielkosc) {
+		System.out.println("Wygenerowano planszę o wielkości 7");
+	}
+
+	public void generate9(int wielkosc) {
+		System.out.println("Wygenerowano planszę o wielkości 7");
+	}
 }
--- src/main/utils/Solver.java
+++ src/main/utils/Solver.java
@@ -1,4 +1,9 @@
 package main.utils;
 
 public class Solver {
+	
+	boolean isSolvable(char[][] tableToTest) {
+		//TODO nie zawsze true
+		return true;
+	}
 }
