23f5542fa0871de459a9e8515cd2f30e8c255667
alis1 <aaaaav@Workstation>
2023-12-26 11:56:26 +0100

    Loops in switch cases

--- src/main/utils/Generator.java
+++ src/main/utils/Generator.java
@@ -5,17 +5,19 @@ import java.util.Random;
 public class Generator {
 
 	static Random random = new Random();
+	Solver solver = new Solver();
 
 	static char[][] board5 = new char[5][5];
 	static char[][] board7 = new char[7][7];
 	static char[][] board9 = new char[9][9];
 
 	public Generator() {
-		System.out.println("Nie wprowadzono wielkości planszy");
 	}
 
-	public Generator(int rozmiar) {
-		//TODO jakis switch
+	public Generator(int size) {
+		// TODO Tymczasowe new Generator
+		Generator generator = new Generator();
+		generator.generate(size);
 	}
 
 	public static void main(String[] args) {
@@ -23,18 +25,54 @@ public class Generator {
 		System.out.println(board5);
 		System.out.println(board7);
 		System.out.println(board9);
+		// TODO Tymczasowe new Generator
+		Generator generator = new Generator();
+		generator.generate(9);
+		System.out.println(board9[4][4]);
 
 	}
 
-	public void generate5(int wielkosc) {
-		System.out.println("Wygenerowano planszę o wielkości 5");
-	}
+	void generate(int size) {
+		switch (size) {
 
-	public void generate7(int wielkosc) {
-		System.out.println("Wygenerowano planszę o wielkości 7");
-	}
+		case 5:
+			do {
+				for (char[] row : board5) {
+					for (int i = 0; i < row.length; i++) {
+						//TODO
+						row[i] = '?';
+					}
+				}
+			} while (!solver.isSolvable(board5));
+			System.out.println("Wygenerowano planszę o wielkości 5");
+			break;
 
-	public void generate9(int wielkosc) {
-		System.out.println("Wygenerowano planszę o wielkości 7");
+		case 7:
+			do {
+				for (char[] row : board7) {
+					for (int i = 0; i < row.length; i++) {
+						//TODO
+						row[i] = '?';
+					}
+				}
+			} while (!solver.isSolvable(board7));
+			System.out.println("Wygenerowano planszę o wielkości 7");
+			break;
+
+		case 9:
+			do {
+				for (char[] row : board9) {
+					for (int i = 0; i < row.length; i++) {
+						//TODO
+						row[i] = '?';
+					}
+				}
+			} while (!solver.isSolvable(board7));
+			System.out.println("Wygenerowano planszę o wielkości 9");
+			break;
+
+		default:
+			System.out.println("Wprowadzono niepoprawną wielkość planszy");
+		}
 	}
 }
