7cf78da337844ee77e9886c152af6c0b73ca3168
alis1 <aaaaav@Workstation>
2023-12-31 17:29:49 +0100

    availableChars tables for standard and 1st rows + code to handle them in void generate

--- src/main/utils/Generator.java
+++ src/main/utils/Generator.java
@@ -11,17 +11,79 @@ public class Generator {
 	static char[][] board7 = new char[7][7];
 	static char[][] board9 = new char[9][9];
 
+	// Zestawy możliwych charów w tablicy oraz co może być po czym
+
 	static char[] availableCharsCommon = { '-', '|', '0' };
 	static char[] availableCharsCurves = { 'F', 'T', 'L', 'J' };
 
+	// Co można wstawić na początek wiersza?
 	static char[] availableCharsStartRow = { '|', '0', 'F', 'L' };
 
+	// Co może być dalej?
 	// -
 	static char[] availableCharsAfterMinus = { '-', '0', 'T', 'J' };
 	// |
 	static char[] availableCharsAfterPipe = { '|', '0', 'F', 'L' };
 	// Number
 	static char[] availableCharsAfterNumber = { '-', '|', '0', 'T', 'J' };
+
+	// Aby nie było kwadratów 2x2 to po F nie może być T bo skręcają w tym samym
+	// kierunku (analogocznie dla pozostałych)
+	// F
+	static char[] availableCharsAfterF = { '-', '0', 'J' };
+	// T
+	static char[] availableCharsAfterT = { '|', '0', 'L' };
+	// L
+	static char[] availableCharsAfterL = { '-', '0', 'T' };
+	// J
+	static char[] availableCharsAfterJ = { '|', '0', 'F' };
+
+	// Jak będzie można zakończyć wiersz? Inaczej dla każdego znaku poprzedzającego!
+	static char[] availableCharsEndRow = { '|', '0', 'T', 'J' };
+	static char[] availableCharsEndRowAfterMinus = { '0', 'T', 'J' };
+	static char[] availableCharsEndRowAfterPipe = { '|', '0', 'T', 'J' };
+	static char[] availableCharsEndRowAfterNumber = { '|', '0', 'T', 'J' };
+	static char[] availableCharsEndRowAfterF = { '0', 'J' };
+	static char[] availableCharsEndRowAfterT = { '|', '0' };
+	static char[] availableCharsEndRowAfterL = { '0', 'T' };
+	static char[] availableCharsEndRowAfterJ = { '|', '0' };
+
+	// PIERWSZY WIERSZ
+	// Dla pierwszego wiersza nie może być |, J, L
+
+	// Co można wstawić na początek wiersza?
+	static char[] availableChars1stStartRow = { '0', 'F' };
+
+	// Co może być dalej?
+	// -
+	static char[] availableChars1stAfterMinus = { '-', '0', 'T' };
+	// |
+	static char[] availableChars1stAfterPipe = { '0', 'F' };
+	// Number
+	static char[] availableChars1stAfterNumber = { '-', '0', 'T' };
+
+	// Aby nie było kwadratów 2x2 to po F nie może być T bo skręcają w tym samym
+	// kierunku (analogocznie dla pozostałych)
+	// F
+	static char[] availableChars1stAfterF = { '-', '0' };
+	// T
+	static char[] availableChars1stAfterT = { '0' };
+	// L
+	static char[] availableChars1stAfterL = { '-', '0', 'T' };
+	// J
+	static char[] availableChars1stAfterJ = { '0', 'F' };
+
+	// Jak będzie można zakończyć wiersz? Inaczej dla każdego znaku poprzedzającego!
+	static char[] availableChars1stEndRow = { '0', 'T' };
+	static char[] availableChars1stEndRowAfterMinus = { '0', 'T' };
+	static char[] availableChars1stEndRowAfterPipe = { '0', 'T' };
+	static char[] availableChars1stEndRowAfterNumber = { '0', 'T' };
+	static char[] availableChars1stEndRowAfterF = { '0', 'J' };
+	static char[] availableChars1stEndRowAfterT = { '0' };
+	static char[] availableChars1stEndRowAfterL = { '0', 'T' };
+	static char[] availableChars1stEndRowAfterJ = { '0' };
+	
+	// TODO OSTATNI WIERSZ
 
 	public Generator() {
 	}
@@ -45,6 +107,7 @@ public class Generator {
 			for (int i = 0; i < board5.length; i++) {
 				System.out.println(board5[j][i]);
 			}
+			System.out.println(" ");
 		}
 
 	}
@@ -54,27 +117,76 @@ public class Generator {
 
 		case 5:
 			do {
+				boolean is1stRowDone = false;
 				for (char[] row : board5) {
-					for (int i = 1; i < row.length; i++) {
+					
+					if (!is1stRowDone) {
+						row[0] = availableChars1stStartRow[random.nextInt(availableChars1stStartRow.length)];
+
+						for (int i = 1; i < row.length - 1; i++) {
+
+							switch (row[i - 1]) {
+							case '-':
+								row[i] = availableChars1stAfterMinus[random
+										.nextInt(availableChars1stAfterMinus.length)];
+								break;
+							case '|':
+								row[i] = availableChars1stAfterPipe[random.nextInt(availableChars1stAfterPipe.length)];
+								break;
+							case '0':
+								row[i] = availableChars1stAfterNumber[random
+										.nextInt(availableChars1stAfterNumber.length)];
+								break;
+							case 'F':
+								row[i] = availableChars1stAfterF[random.nextInt(availableChars1stAfterF.length)];
+								break;
+							case 'T':
+								row[i] = availableChars1stAfterT[random.nextInt(availableChars1stAfterT.length)];
+								break;
+							case 'L':
+								row[i] = availableChars1stAfterL[random.nextInt(availableChars1stAfterL.length)];
+								break;
+							case 'J':
+								row[i] = availableChars1stAfterJ[random.nextInt(availableChars1stAfterJ.length)];
+								break;
+							}
+						}
+						row[row.length - 1] = availableCharsEndRow[random.nextInt(availableCharsEndRow.length)];
+						is1stRowDone = true;
+						continue;
+						
+					} else {
+
 						row[0] = availableCharsStartRow[random.nextInt(availableCharsStartRow.length)];
 
-						switch (row[i - 1]) {
-						case 'F':
-						case 'L':
-						case '-':
-							row[i] = availableCharsAfterMinus[random.nextInt(availableCharsAfterMinus.length)];
-							break;
-						case 'T':
-						case 'J':
-						case '|':
-							row[i] = availableCharsAfterPipe[random.nextInt(availableCharsAfterPipe.length)];
-							break;
-						case '0':
-							row[i] = availableCharsAfterNumber[random.nextInt(availableCharsAfterNumber.length)];
-							break;
+						for (int i = 1; i < row.length - 1; i++) {
 
+							switch (row[i - 1]) {
+							case '-':
+								row[i] = availableCharsAfterMinus[random.nextInt(availableCharsAfterMinus.length)];
+								break;
+							case '|':
+								row[i] = availableCharsAfterPipe[random.nextInt(availableCharsAfterPipe.length)];
+								break;
+							case '0':
+								row[i] = availableCharsAfterNumber[random.nextInt(availableCharsAfterNumber.length)];
+								break;
+							case 'F':
+								row[i] = availableCharsAfterF[random.nextInt(availableCharsAfterF.length)];
+								break;
+							case 'T':
+								row[i] = availableCharsAfterT[random.nextInt(availableCharsAfterT.length)];
+								break;
+							case 'L':
+								row[i] = availableCharsAfterL[random.nextInt(availableCharsAfterL.length)];
+								break;
+							case 'J':
+								row[i] = availableCharsAfterJ[random.nextInt(availableCharsAfterJ.length)];
+								break;
+							}
 						}
 
+						row[row.length - 1] = availableCharsEndRow[random.nextInt(availableCharsEndRow.length)];
 					}
 				}
 			} while (!solver.isSolvable(board5));
