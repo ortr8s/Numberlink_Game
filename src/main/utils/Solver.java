0c35f2ed40ee108b5497c5f6ecfe1aa0dd514b8f
alis1 <aaaaav@Workstation>
2024-01-01 21:28:13 +0100

    Removed C - shaped paths from possible outcomes Testing numbers for outgoing connections Various table error fixes

--- src/main/utils/Solver.java
+++ src/main/utils/Solver.java
@@ -25,8 +25,80 @@ public class Solver {
 		priorityQueue.add(startUnit);
 		return false;
 	}
+	@SuppressWarnings("finally")
 	boolean isSolvable(char[][] tableToTest) {
-		//TODO nie zawsze true
-		return true;
+		boolean isnotWrong = true;
+		int countingTillTheEnd = 0;
+		for (char[] row : tableToTest) {
+			for (int i = 0; i < row.length; i++) { 
+				boolean occupied = false;
+				if (row[i] == '0') {
+					for (int condition = 0; condition < 5; condition++) {
+						System.out.println("Robia sie warunki");
+						switch (condition) {
+						case 0:
+							System.out.println("Robi sie 0");
+							try {
+								if (Generator.hasMandatoryBottomConnection(tableToTest[countingTillTheEnd - 1][i])) {
+									if (occupied == true) {
+										isnotWrong = false;
+									}
+									occupied = true;
+								} 
+							} finally {
+								continue;
+							}
+						case 1:
+							System.out.println("Robi sie 1");
+							try {
+								if (Generator.hasMandatoryConnectionUP(tableToTest[countingTillTheEnd + 1][i])) {
+									if (occupied == true) {
+										isnotWrong = false;
+									}
+									occupied = true;
+								} 
+							} finally {
+								continue;
+							} 
+						case 2:
+							System.out.println("Robi sie 2");
+							try {
+								if (Generator.hasMandatoryConnectionLeft(tableToTest[countingTillTheEnd][i + 1])) {
+									if (occupied == true) {
+										isnotWrong = false;
+									}
+									occupied = true;
+								} 
+							} finally {
+								continue;
+							} 
+						case 3:
+							System.out.println("Robi sie 3");
+							try {
+								if (Generator.hasMandatoryConnectionRight(tableToTest[countingTillTheEnd][i - 1])) {
+									if (occupied == true) {
+										isnotWrong = false;
+									}
+									occupied = true;
+								} 
+							} finally {
+								continue;
+							} 
+						case 4:
+							System.out.println("Robi sie 4");
+							if (occupied == false) {
+								isnotWrong = false;
+							}
+						}
+					}
+				}
+			}
+			countingTillTheEnd++;
+		}
+		if (!isnotWrong) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
