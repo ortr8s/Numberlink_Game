ffc4589087a4883f2b7d6c9c0160259cc2351615
Adrian Lis <01187024@pw.edu.pl>
2024-01-08 09:50:55 +0100

    Generator raw file saving

--- src/main/utils/Saving.java
+++ src/main/utils/Saving.java
@@ -1,4 +1,38 @@
 package main.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Saving {
+	
+	public static void main(String[] args) {
+    	
+    }
+    
+    public static void rawGeneratorTableToTxt(char[][] generatedBoard) {
+    	
+    	try {
+            FileWriter txtWriter = new FileWriter("raw_generator_table.txt");
+
+            for (int j = 0; j < generatedBoard.length; j++) {
+				for (int i = 0; i < generatedBoard.length; i++) {
+					System.out.print(generatedBoard[j][i]);
+					txtWriter.write(generatedBoard[j][i]);
+					System.out.print("  ");
+					txtWriter.write("  ");
+				}
+				System.out.println(" ");
+				txtWriter.write("\n");
+			}
+            
+            txtWriter.close();
+
+            System.out.println("Text saved successfully!");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the text to the file.");
+            e.printStackTrace();
+        }
+    	
+    }
+    
 }
